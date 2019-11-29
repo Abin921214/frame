8eafbaad629b909bc156f79451ad13958d150189
Abin921214 <l>
2019-11-29 11:03:08 +0800

    frist

---
+++ frame01/src/test/java/com/jzhl/frame01/Frame01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jzhl.frame01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Frame01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
